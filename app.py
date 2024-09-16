9bb0cec42e64db5e186f5c436c5132283a971170
Aventus <107351932+Aventus007@users.noreply.github.com>
2024-09-16 17:33:02 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-(X_train, Y_train), (X_test, Y_test) = datasets.cifar10.load_data()
-
-print(X_train.shape)
